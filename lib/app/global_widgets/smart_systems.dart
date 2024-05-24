b56dee0f3aad329416c2734983bf671585e66038
MilesXu <kanonxmm@163.com>
2024-05-24 16:53:24 +0800

    fix for ios

--- lib/app/global_widgets/smart_systems.dart
+++ lib/app/global_widgets/smart_systems.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:home_fi/app/global_widgets/animated_switch.dart';
--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -5,14 +5,14 @@ import 'package:get/get.dart';
 import 'package:home_fi/app/modules/home/views/home_view.dart';
 
 class SplashScreenController extends GetxController
-    with SingleGetTickerProviderMixin {
+    with GetSingleTickerProviderStateMixin {
   // animation controller for lottie
   late AnimationController animationController;
 
   @override
   void onInit() {
     super.onInit();
-    // spalsh animation config
+    // splash animation config
     animationController = AnimationController(
       duration: Duration(seconds: 4),
       vsync: this,
